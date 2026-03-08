f544e433a352bf17e3600ac63478a30e0c796e98
Syed Naqvi <znac566@live.rhul.ac.uk>
2026-03-08 21:29:01 +0000

    DEL

--- backend/src/main/java/uk/ac/rhul/cs2810/repository/MenuItemCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.rhul.cs2810.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.ac.rhul.cs2810.model.MenuItemCategory;
-
-public interface MenuItemCategoryRepository extends CrudRepository<MenuItemCategory, Long> {}
--- backend/src/main/java/uk/ac/rhul/cs2810/repository/MenuItemRepository.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.rhul.cs2810.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.ac.rhul.cs2810.model.MenuItem;
-
-public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {}
